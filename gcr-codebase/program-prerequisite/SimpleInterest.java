987275242c49ce9357c090d22ababe1ced9b33a1
Ali Ahmad <saquibkamal95@gmail.com>
2025-12-16 21:03:10 +0530

    [Ali Ahmad] : added a program to calculate the Simple Interest

---
+++ gcr-codebase/program-prerequisite/SimpleInterest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Principal amount: ");
+        int principal = sc.nextInt();
+
+        System.out.print("Enter Rate of Interest: ");
+        int rate = sc.nextInt();
+
+        System.out.print("Enter Time (in years): ");
+        int time = sc.nextInt();
+
+        int simpleInterest = (principal * rate * time) / 100;
+
+        System.out.println("Simple Interest is: " + simpleInterest);
+
+    }
+}

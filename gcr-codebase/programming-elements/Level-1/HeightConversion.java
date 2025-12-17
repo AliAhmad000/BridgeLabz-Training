44130c59a1c7d55e5f9faf993c38675a0ee0bfbf
Ali Ahmad <saquibkamal95@gmail.com>
2025-12-17 14:25:32 +0530

    [Ali Ahmad] :  added a program to Convert the Heights into inches and feet

---
+++ gcr-codebase/programming-elements/Level-1/HeightConversion.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HeightConversion {
+    public static void main(String[] args) {
+        double heightCm;
+        double totalInches;
+        int feet;
+        double inches;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your height in centimeters: ");
+        heightCm = input.nextDouble();
+
+        totalInches = heightCm / 2.54;   // convert cm to inches
+        feet = (int) (totalInches / 12); // get feet
+        inches = totalInches % 12;       // remaining inches
+
+        System.out.println( "Your Height in cm is " + heightCm + " while in feet is " + feet + " and inches is " + inches );
+    }
+}

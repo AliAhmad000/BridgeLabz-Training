4e745a23b50e35a8f79bc4a98f814d9cb58e9f18
Ali Ahmad <saquibkamal95@gmail.com>
2025-12-17 13:04:24 +0530

    [Ali Ahmad] :  added a program to calculate the discounted fees

---
+++ gcr-codebase/programming-elements/Level-1/DiscountFee.java
@@ -0,0 +1,16 @@
+public class DiscountFee
+{
+    // Main method: program execution starts here
+    public static void main(String args[])
+    {
+      int fee = 125000;
+      int discountPercent = 10;
+      int discount = (fee/100)*10;
+      int discountedAmount = fee - discount;
+
+      //Display the output
+      System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR" + discountedAmount );
+
+
+    }
+}

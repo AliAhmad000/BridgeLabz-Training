5211c8967bfe046f457acd4475f5e0245095d78a
Ali Ahmad <saquibkamal95@gmail.com>
2025-12-18 14:54:45 +0530

    [Ali Ahmad ] : added a program to calculate the sum of user unit he preses zero, using while loop.

---
+++ gcr-codebase/control-flows/Level-1/SumUntilZeroWhileLoop.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class SumUntilZeroWhileLoop{
+    public static void main(String args[]){
+        double total = 0.0, value;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value : ");
+        value = sc.nextDouble();
+        
+        while (true){
+            if(value==0 || value<0){
+                break;
+            }
+            else{
+                total += value; 
+                System.out.println("Enter the value : ");
+                value = sc.nextDouble();
+            }
+            }
+         
+         System.out.println("The total sum of the input values is : " + total);
+    }
+}

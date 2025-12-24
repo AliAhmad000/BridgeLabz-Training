ae2fabca0c5a8d28a98d8b76a69661b15ca9692c
Ali Ahmad <saquibkamal95@gmail.com>
2025-12-24 15:09:19 +0530

    [Ali Ahmad] : Changed the path of extra folders

--- gcr-codebase/methods/Level-1/CalculateSimpleInterest.java
+++ gcr-codebase/methods/Level-1/CalculateSimpleInterest.java
@@ -8,7 +8,7 @@ class CalculateSimpleInterest{
 }
   
    public static void main(String []args){
-       Scanner scanner=new Scanner(System.in);
+       Scanner scanner=new Scanner(System.in) ;
 
        //Taking Principal,Rate of interest and time in months from the user for calculating simple interest 
        System.out.println("Enter the Principal amount");
@@ -16,7 +16,7 @@ class CalculateSimpleInterest{
        System.out.println("Enter the Rate of interest");
        double rate=scanner.nextInt();
        System.out.println("Enter Time in Years ");
-       double time=scanner.nextInt();
+       double time=scanner.nextInt() ;
        
       //  calling the method for calculating simple interest
       CalculateSimpleInterest.calculateSimpleInterest(principal,rate,time);

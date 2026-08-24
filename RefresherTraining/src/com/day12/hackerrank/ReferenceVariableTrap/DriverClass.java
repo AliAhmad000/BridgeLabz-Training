adda202675998474a0cc6ae0e85741099cf7310e
Ali Ahmad <saquibkamal95@gmail.com>
2026-08-24 09:15:45 +0530

    Added HackerRank solution.

---
+++ RefresherTraining/src/com/day12/hackerrank/ReferenceVariableTrap/DriverClass.java
@@ -0,0 +1,13 @@
+package com.day12.hackerrank.ReferenceVariableTrap;
+
+public class DriverClass {
+	public static void main(String[] args) {
+		Rectangle rect1 = new Rectangle(4, 5);
+//      Rectangle rect2 = rect1;
+		Rectangle rect2 = new Rectangle(rect1);
+
+		rect1.displayRectangleDetails();
+		rect2.scale(2);
+		rect1.displayRectangleDetails();
+	}
+}
---
+++ RefresherTraining/src/com/day16/hackerrank/StaticInitializationBlock.java
@@ -0,0 +1,28 @@
+package com.day16.hackerrank;
+
+import java.util.*;
+
+public class StaticInitializationBlock {
+    static int b;
+    static int h;
+    static boolean flag = true;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+
+        b = sc.nextInt();
+        h = sc.nextInt();
+
+        if (b <= 0 || h <= 0) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = b * h;
+            System.out.println(area);
+        }
+    }
+}
